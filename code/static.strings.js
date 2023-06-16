6687823569d89861ea85bceec4039933f6fb168e
HyperPaint <dioman172@yandex.ru>
2023-06-17 00:47:19 +1000

    Update static.strings.js

--- code/static.strings.js
+++ code/static.strings.js
@@ -1,4 +1,4 @@
-const { errorCodeToText } = require("./static.errors");
+const staticErrors = require("./static.errors");
 
 const staticStrings = {
     /**
@@ -17,20 +17,20 @@ const staticStrings = {
      * @type {Function[]}
      */
     console: {
-        notRealized: (object, targetId, errorCode = 0) => `Объект ${object.id} не может работать с объектом ${targetId}, функция не реализована, ошибка ${errorCodeToText(errorCode)}`,
+        notRealized: (object, targetId, errorCode = 0) => `Объект ${object.id} не может работать с объектом ${targetId}, функция не реализована, ошибка ${staticErrors.errorCodeToText(errorCode)}`,
 
-        creepCannotAttack: (creep, targetId, errorCode) => `Крип ${creep.name} не может атаковать цель ${targetId}, ошибка ${errorCodeToText(errorCode)}`,
-        creepCannotBuild: (creep, targetId, errorCode) => `Крип ${creep.name} не может строить объект ${targetId}, ошибка ${errorCodeToText(errorCode)}`,
-        creepCannotClaimController: (creep, targetId, errorCode) => `Крип ${creep.name} не может захватить контроллер ${targetId}, ошибка ${errorCodeToText(errorCode)}`,
-        creepCannotDismatle: (creep, targetId, errorCode) => `Крип ${creep.name} не может разбирать объект ${targetId}, ошибка ${errorCodeToText(errorCode)}`,
-        creepCannotGenerateSafeMode: (creep, targetId, errorCode) => `Крип ${creep.name} не может создать безопасный режим в контроллере ${targetId}, ошибка ${errorCodeToText(errorCode)}`,
-        creepCannotHarvest: (creep, targetId, errorCode) => `Крип ${creep.name} не может добывать ${targetId}, ошибка ${errorCodeToText(errorCode)}`,
-        creepCannotHeal: (creep, targetId, errorCode) => `Крип ${creep.name} не может лечить ${targetId}, ошибка ${errorCodeToText(errorCode)}`,
-        creepCannotPickup: (creep, targetId, errorCode) => `Крип ${creep.name} не может подбирать ${targetId}, ошибка ${errorCodeToText(errorCode)}`,
-        creepCannotRepair: (creep, targetId, errorCode) => `Крип ${creep.name} не может чинить ${targetId}, ошибка ${errorCodeToText(errorCode)}`,
-        creepCannotTransfer: (creep, targetId, errorCode) => `Крип ${creep.name} не может отдавать в ${targetId}, ошибка ${errorCodeToText(errorCode)}`,
-        creepCannotUpgradeController: (creep, targetId, errorCode) => `Крип ${creep.name} не может улучать контроллер ${targetId}, ошибка ${errorCodeToText(errorCode)}`,
-        creepCannotWithdraw: (creep, targetId, errorCode) => `Крип ${creep.name} не может забирать из ${targetId}, ошибка ${errorCodeToText(errorCode)}`,
+        creepCannotAttack: (creep, targetId, errorCode) => `Крип ${creep.name} не может атаковать цель ${targetId}, ошибка ${staticErrors.errorCodeToText(errorCode)}`,
+        creepCannotBuild: (creep, targetId, errorCode) => `Крип ${creep.name} не может строить объект ${targetId}, ошибка ${staticErrors.errorCodeToText(errorCode)}`,
+        creepCannotClaimController: (creep, targetId, errorCode) => `Крип ${creep.name} не может захватить контроллер ${targetId}, ошибка ${staticErrors.errorCodeToText(errorCode)}`,
+        creepCannotDismatle: (creep, targetId, errorCode) => `Крип ${creep.name} не может разбирать объект ${targetId}, ошибка ${staticErrors.errorCodeToText(errorCode)}`,
+        creepCannotGenerateSafeMode: (creep, targetId, errorCode) => `Крип ${creep.name} не может создать безопасный режим в контроллере ${targetId}, ошибка ${staticErrors.errorCodeToText(errorCode)}`,
+        creepCannotHarvest: (creep, targetId, errorCode) => `Крип ${creep.name} не может добывать ${targetId}, ошибка ${staticErrors.errorCodeToText(errorCode)}`,
+        creepCannotHeal: (creep, targetId, errorCode) => `Крип ${creep.name} не может лечить ${targetId}, ошибка ${staticErrors.errorCodeToText(errorCode)}`,
+        creepCannotPickup: (creep, targetId, errorCode) => `Крип ${creep.name} не может подбирать ${targetId}, ошибка ${staticErrors.errorCodeToText(errorCode)}`,
+        creepCannotRepair: (creep, targetId, errorCode) => `Крип ${creep.name} не может чинить ${targetId}, ошибка ${staticErrors.errorCodeToText(errorCode)}`,
+        creepCannotTransfer: (creep, targetId, errorCode) => `Крип ${creep.name} не может отдавать в ${targetId}, ошибка ${staticErrors.errorCodeToText(errorCode)}`,
+        creepCannotUpgradeController: (creep, targetId, errorCode) => `Крип ${creep.name} не может улучать контроллер ${targetId}, ошибка ${staticErrors.errorCodeToText(errorCode)}`,
+        creepCannotWithdraw: (creep, targetId, errorCode) => `Крип ${creep.name} не может забирать из ${targetId}, ошибка ${staticErrors.errorCodeToText(errorCode)}`,
     },
 }
 
